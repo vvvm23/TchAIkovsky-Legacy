0741325670e9f169a89e465dcddd046f7cc4e84b
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-09-12 15:23:21 +0100

    DataGenerator framework

---
+++ data_generator.py
@@ -0,0 +1,20 @@
+import numpy as numpy
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+    def __data_generation(self):
+        pass
+
+
