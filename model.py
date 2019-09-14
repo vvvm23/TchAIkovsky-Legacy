aa87e237c6fa3b8ee369ed5761b56934c04598a5
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-09-14 16:18:02 +0100

    added basic idea of network structure

--- model.py
+++ model.py
@@ -1,8 +1,23 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, Flatten
 
-# TODO: Add the other layers
-def create_model():
+# TODO: Parameterise this properly
+def create_model(input_shape):
     model = Sequential()
+    model.add(LSTM(128, input_shape=input_shape, return_sequences=True))
+    model.add(Dropout(0.5))
 
+    model.add(LSTM(64, return_sequences=True))
+    model.add(Dropout(0.5))
+
+    model.add(LSTM(64, return_sequences=True))
+    model.add(Dropout(0.5))
+
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(176, activation='sigmoid'))
     return model
+   
+model = create_model((100, 176))
+model.summary()
