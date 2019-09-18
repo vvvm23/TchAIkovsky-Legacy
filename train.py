34004159cf1af86bdcca6cf723266d7c09f758de
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-09-18 18:39:22 +0100

    mulithreading ID List generation

--- train.py
+++ train.py
@@ -12,20 +12,35 @@ import time
 
 import random
 
-# Function to generate ID_list from metafile
-# TODO: Maybe multithread this using ThreadPoolExecutor. Right now this is annoyingly slow.
-def ID_list_generation(meta_file, seq_size):
+import threading
+import concurrent.futures
+ 
+def multi_ID_list_generation(meta_file, seq_size, workers=8):
+    worker_output = [[]] * 8
+
+    def _worker_ID_list_generation(arg):
+        fl_pairs = arg[0]
+        worker_id = arg[1]
+        for pair in fl_pairs:
+            worker_output[worker_id] = worker_output[worker_id] + [(pair[0], i) for i in range(pair[1] - seq_size)]
+
     df = pd.read_csv(meta_file, header=None, usecols=[0,1])
     fl_pairs = list(zip(df[0].tolist(), df[1].tolist()))
-    _ID_list = []
 
-    for pair in tqdm(fl_pairs):
-        _ID_list = _ID_list + [(pair[0], i) for i in range(pair[1] - seq_size)]
+    div = len(fl_pairs) // workers
 
-    return _ID_list
+    executor_args = [(fl_pairs[div*i:div*(i+1)], i) for i in range(workers - 1)]
+    executor_args.append((fl_pairs[div*(workers - 1):], workers-1))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        executor.map(_worker_ID_list_generation, executor_args)
+
+    return [x for y in worker_output for x in y]
 
 print("Generating ID List.. ", end='')
-ID_list = ID_list_generation("./preprocessing/np_out/META.csv", 200)
+#ID_list = ID_list_generation("./preprocessing/np_out/META.csv", 200)
+ID_list = multi_ID_list_generation("./preprocessing/np_out/META.csv", 200, workers=8)
+
 random.shuffle(ID_list)
 print("Done.")
 
@@ -36,7 +51,6 @@ params = {
 
 print("Creating Model.. ", end='')
 model = create_model((200, 176))
-model.summary()
 print("Done.")
 
 # ID split is placeholder for now, potentially use maestro metadata to split?
