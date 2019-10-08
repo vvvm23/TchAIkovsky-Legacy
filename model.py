f796b9369e133e1d1b005e0edcce42193273bc20
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-10-08 16:16:08 +0100

    changed model to incorporate Embedding Layer

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Flatten, TimeDistributed, CuDNNLSTM
+from keras.layers import LSTM, Dense, Dropout, Flatten, TimeDistributed, CuDNNLSTM, Embedding
 
+'''
 # TODO: Parameterise this properly
 def create_model(input_shape):
     model = Sequential()
@@ -20,5 +21,23 @@ def create_model(input_shape):
 
     #model.add(Flatten())
     model.add(Dense(176, activation='sigmoid'))
+
+    return model
+'''
+# input.shape = (batch_size, sequence_length)
+def create_model(seq_length, dict_size, vocab_size):
+    model = Sequential()
+    model.add(Embedding(dict_size, vocab_size, input_length=seq_length))
+
+    model.add(CuDNNLSTM(64, return_sequences=True))
+    model.add(Dropout(0.5))
+
+    model.add(CuDNNLSTM(64, return_sequences=True))
+    model.add(Dropout(0.5))
+
+    model.add(CuDNNLSTM(64, return_sequences=False))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(dict_size, activation='sigmoid')) 
 
     return model
